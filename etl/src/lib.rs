d40f1e4e26a85bb77f7509305fe6bc956c7fc177
Tyler Earls <tyler.a.earls@gmail.com>
2021-01-28 22:22:12 -0600

    change var name to not use char

--- etl/src/lib.rs
+++ etl/src/lib.rs
@@ -3,8 +3,8 @@ use std::collections::BTreeMap;
 pub fn transform(h: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
     let mut result: BTreeMap<char, i32> = BTreeMap::new();
     for (score, vec_chars) in h {
-        for char in vec_chars {
-            result.insert(char.to_ascii_lowercase(), *score);
+        for vec_char in vec_chars {
+            result.insert(vec_char.to_ascii_lowercase(), *score);
         }
     }
     result
