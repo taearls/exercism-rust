f5720599ab4d1ac6cefbacd4fa99f89da09dd2a2
Tyler Earls <tyler.a.earls@gmail.com>
2021-01-31 15:12:50 -0600

    refactor to use a while loop

--- binary-search/src/lib.rs
+++ binary-search/src/lib.rs
@@ -6,31 +6,19 @@ where
     let array_ref = array.as_ref();
     let mut left = 0;
     let mut right = array_ref.len();
+    
     // if len is zero, subtracting 1 will create an overflow error bc it's a usize
     if right == 0 {
         return None;
     }
-    loop {
-        if left > right {
-            break;
-        }
-
+    while left < right {
         let mid = (left + right) / 2;
         let val = &array_ref[mid];
-        if *val < key {
-            if mid == array_ref.len() - 1 {
-                break;
-            } else {
-                left = mid + 1;
-            }
-        } else if *val > key {
-            // if mid is 0 and the number is greater than the target key, 
-            // the target is smaller than the smallest element in the array and we should return None
-            if mid == 0 { 
-                break; 
-            } else {
-                right = mid - 1;
-            }
+        if key > *val {
+            // if the target value is greater, we only need to search to the right
+            left = mid + 1;
+        } else if key < *val {
+            right = mid;
         } else {
             return Some(mid);
         }
