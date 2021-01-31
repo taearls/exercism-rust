c535ec1dbe5152b97e0c07972e252bf4070e7f5a
Tyler Earls <tyler.a.earls@gmail.com>
2021-01-31 15:18:00 -0600

    refactor to use match arms

--- binary-search/src/lib.rs
+++ binary-search/src/lib.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 pub fn find<T, U>(array: T, key: U) -> Option<usize>
 where
     T: AsRef<[U]>,
@@ -7,20 +9,16 @@ where
     let mut left = 0;
     let mut right = array_ref.len();
 
-    // if len is zero, subtracting 1 will create an overflow error bc it's a usize
-    if right == 0 {
-        return None;
-    }
-    while left < right {
-        let mid = (left + right) / 2;
-        let val = &array_ref[mid];
-        if key > *val {
-            // if the target value is greater, we only need to search to the right
-            left = mid + 1;
-        } else if key < *val {
-            right = mid;
-        } else {
-            return Some(mid);
+    // only perform search if len is greater than 0
+    if right > 0 {
+        while left < right {
+            let mid = (left + right) / 2;
+            let val = &array_ref[mid];
+            match key.cmp(val) {
+                Ordering::Greater => left = mid + 1,
+                Ordering::Less => right = mid,
+                Ordering::Equal => return Some(mid),
+            }
         }
     }
     None
