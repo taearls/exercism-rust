46637ab10237ef759f00b3f3c5553bef8820afac
Tyler Earls <tyler.a.earls@gmail.com>
2021-01-31 15:03:44 -0600

    get generic types working with all tests

--- binary-search/src/lib.rs
+++ binary-search/src/lib.rs
@@ -1,33 +1,38 @@
-pub fn find(array: &[i32], key: i32) -> Option<usize> {
+pub fn find<T, U> (array: T, key: U) -> Option<usize>
+where 
+    T: AsRef<[U]>,
+    U: Ord, 
+{
+    let array_ref = array.as_ref();
+    let mut left = 0;
+    let mut right = array_ref.len();
     // if len is zero, subtracting 1 will create an overflow error bc it's a usize
-    if array.len() == 0 {
+    if right == 0 {
         return None;
     }
-    let mut left = 0;
-    let mut right = array.len() - 1;
     loop {
         if left > right {
             break;
         }
 
-        let mid = (((left + right) as f32) / 2.0).floor() as usize;
-        match array.get(mid) {
-            Some(num) => {
-                if *num < key {
-                    left = mid + 1;
-                } else if *num > key {
-                    // if mid is 0 and the number is greater than the target key, 
-                    // the target is smaller than the smallest element in the array and we should return None
-                    if mid == 0 { 
-                        break; 
-                    } else {
-                        right = mid - 1;
-                    }
-                } else {
-                    return Some(mid);
-                }
+        let mid = (left + right) / 2;
+        let val = &array_ref[mid];
+        if *val < key {
+            if mid == array_ref.len() - 1 {
+                break;
+            } else {
+                left = mid + 1;
             }
-            None => break,
+        } else if *val > key {
+            // if mid is 0 and the number is greater than the target key, 
+            // the target is smaller than the smallest element in the array and we should return None
+            if mid == 0 { 
+                break; 
+            } else {
+                right = mid - 1;
+            }
+        } else {
+            return Some(mid);
         }
     }
     None
--- binary-search/tests/binary-search.rs
+++ binary-search/tests/binary-search.rs
@@ -72,15 +72,11 @@ fn nothing_is_found_when_the_left_and_right_bounds_cross() {
 }
 
 #[test]
-#[ignore]
-#[cfg(feature = "generic")]
 fn works_for_arrays() {
     assert_eq!(find([6], 6), Some(0));
 }
 
 #[test]
-#[ignore]
-#[cfg(feature = "generic")]
 fn works_for_vec() {
     let vector = vec![6];
     assert_eq!(find(&vector, 6), Some(0));
@@ -88,8 +84,6 @@ fn works_for_vec() {
 }
 
 #[test]
-#[ignore]
-#[cfg(feature = "generic")]
 fn works_for_str_elements() {
     assert_eq!(find(["a"], "a"), Some(0));
     assert_eq!(find(["a", "b"], "b"), Some(1));
