4fa13343db3eda2f73a3d1acd5059ad7dbd5fa7d
Tyler Earls <tyler.a.earls@gmail.com>
2021-01-31 21:58:38 -0600

    working solution with all tests passing

--- queen-attack/src/lib.rs
+++ queen-attack/src/lib.rs
@@ -1,31 +1,39 @@
 #[derive(Debug)]
-pub struct ChessPosition;
+pub struct ChessPosition {
+    rank: i32,
+    file: i32,
+}
 
 #[derive(Debug)]
-pub struct Queen;
+pub struct Queen {
+    rank: i32,
+    file: i32,
+}
 
 impl ChessPosition {
     pub fn new(rank: i32, file: i32) -> Option<Self> {
-        unimplemented!(
-            "Construct a ChessPosition struct, given the following rank, file: ({}, {}). If the position is invalid return None.",
-            rank,
-            file
-        );
+        if rank < 0 || rank > 7 || file < 0 || file > 7 {
+            return None;
+        }
+        Some(ChessPosition { rank, file })
     }
 }
 
 impl Queen {
     pub fn new(position: ChessPosition) -> Self {
-        unimplemented!(
-            "Given the chess position {:?}, construct a Queen struct.",
-            position
-        );
+        Queen {
+            rank: position.rank,
+            file: position.file,
+        }
     }
 
     pub fn can_attack(&self, other: &Queen) -> bool {
-        unimplemented!(
-            "Determine if this Queen can attack the other Queen {:?}",
-            other
-        );
+        let mut result = false;
+        if self.rank == other.rank || self.file == other.file {
+            result = true;
+        } else if (self.rank - other.rank).abs() == (self.file - other.file).abs() {
+            result = true;
+        }
+        result
     }
 }
