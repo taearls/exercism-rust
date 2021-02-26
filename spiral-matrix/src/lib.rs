58f801ef678f16e386613d416d1e7622b32d455d
Tyler Earls <tyler.a.earls@gmail.com>
2021-02-25 22:33:15 -0600

    use cargo fmt

--- spiral-matrix/src/lib.rs
+++ spiral-matrix/src/lib.rs
@@ -1,5 +1,4 @@
 pub fn spiral_matrix(size: u32) -> Vec<Vec<u32>> {
-
     let square_width = size as usize;
     let mut matrix: Vec<Vec<u32>> = vec![vec![0; square_width]; square_width];
     if size == 0 {
@@ -17,42 +16,45 @@ pub fn spiral_matrix(size: u32) -> Vec<Vec<u32>> {
     let mut bottom_left_bound = (square_width - 1, 0);
 
     loop {
-        
         if empty_elements == 0 {
             break;
         }
         let (row, col) = current_position;
-        println!("({}, {})", row, col); 
+        println!("({}, {})", row, col);
         println!("direction: {}", direction);
         matrix[row][col] = num;
 
         num += 1;
         empty_elements -= 1;
 
-        if (current_position == top_left_bound) || current_position == top_right_bound || current_position == bottom_right_bound || current_position == bottom_left_bound {
+        if (current_position == top_left_bound)
+            || current_position == top_right_bound
+            || current_position == bottom_right_bound
+            || current_position == bottom_left_bound
+        {
             direction = match direction {
                 Direction::Up => {
                     top_left_bound = (top_left_bound.0 + 1, top_left_bound.0 + 1);
                     Direction::Right
-                },
+                }
                 Direction::Right => {
                     if top_right_bound.1 > 1 {
                         top_right_bound = (top_right_bound.0 + 1, top_right_bound.1 - 1);
                     }
                     Direction::Down
-                },
+                }
                 Direction::Down => {
                     if bottom_right_bound.0 > 1 && bottom_right_bound.1 > 1 {
                         bottom_right_bound = (bottom_right_bound.0 - 1, bottom_right_bound.1 - 1);
                     }
                     Direction::Left
-                },
+                }
                 Direction::Left => {
                     if bottom_left_bound.0 > 1 {
                         bottom_left_bound = (bottom_left_bound.0 - 1, bottom_left_bound.1 + 1);
                     }
                     Direction::Up
-                },
+                }
             };
         }
         current_position = match direction {
