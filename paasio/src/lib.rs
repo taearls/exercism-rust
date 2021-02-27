bd32079499e3aee6ed33846b43f898d86ba7bcd4
Tyler Earls <tyler.a.earls@gmail.com>
2021-02-27 16:21:55 -0600

    update structs readstats and writestats, implement methods that read from new fields

--- paasio/src/lib.rs
+++ paasio/src/lib.rs
@@ -1,7 +1,9 @@
 use std::io::{Read, Result, Write};
 
 pub struct ReadStats<R>{
-    data: R
+    data: R,
+    bytes_through: usize,
+    reads: usize,
 }
 
 impl<R: Read> ReadStats<R> {
@@ -10,7 +12,9 @@ impl<R: Read> ReadStats<R> {
     // wish to remove the leading underscore so the variable is not ignored.
     pub fn new(wrapped: R) -> ReadStats<R> {
         ReadStats {
-            data: wrapped
+            data: wrapped,
+            bytes_through: 0,
+            reads: 0,
         }
     }
 
@@ -19,25 +23,26 @@ impl<R: Read> ReadStats<R> {
     }
 
     pub fn bytes_through(&self) -> usize {
-        unimplemented!()
+        self.bytes_through
     }
 
     pub fn reads(&self) -> usize {
-        unimplemented!()
+        self.reads
     }
 }
 
 impl<R: Read> Read for ReadStats<R> {
     fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
-        // if self.data.len() > buf.len() {
-        //     return Err(buf.len());
-        // }
+        // TODO: handle errors
+        // TODO: update bytes_through, update reads
         Ok(buf.len())
     }
 }
 
 pub struct WriteStats<W>{
     data: W,
+    bytes_through: usize,
+    writes: usize,
 }
 
 impl<W: Write> WriteStats<W> {
@@ -46,7 +51,9 @@ impl<W: Write> WriteStats<W> {
     // wish to remove the leading underscore so the variable is not ignored.
     pub fn new(wrapped: W) -> WriteStats<W> {
         WriteStats {
-            data: wrapped
+            data: wrapped,
+            bytes_through: 0,
+            writes: 0,
         }
     }
 
@@ -55,11 +62,11 @@ impl<W: Write> WriteStats<W> {
     }
 
     pub fn bytes_through(&self) -> usize {
-        unimplemented!()
+        self.bytes_through
     }
 
     pub fn writes(&self) -> usize {
-        unimplemented!()
+        self.writes
     }
 }
 
