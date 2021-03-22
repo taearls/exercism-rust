847a687db43e2014188d63ad8f815645fc342809
Tyler Earls <tyler.a.earls@gmail.com>
2021-03-22 00:58:27 -0500

    add result and set return type

--- saddle-points/src/lib.rs
+++ saddle-points/src/lib.rs
@@ -1,6 +1,4 @@
 pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
-    unimplemented!(
-        "find the saddle points of the following matrix: {:?}",
-        input
-    )
+    let mut result: Vec<(usize, usize)> = Vec::new();
+    result
 }
