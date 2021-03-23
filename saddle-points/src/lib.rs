bf916a3f47852299f030e8916132b207805d328e
Tyler Earls <tyler.a.earls@gmail.com>
2021-03-23 16:00:16 -0500

    all tests passing

--- saddle-points/src/lib.rs
+++ saddle-points/src/lib.rs
@@ -5,44 +5,42 @@ pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
     let row_count = input.len();
     println!("input: {:?}", input);
     println!("row_count: {}", row_count);
+    // map from col number to col
     let mut cols: HashMap<usize, Vec<u64>> = HashMap::new();
     if row_count > 0 {
         for row in 0..row_count {
             let current_row = input.get(row).unwrap().to_vec();
             let max_row_val = match current_row.iter().max() {
                 Some(val) => val,
+                // returns None if current row is empty
                 None => continue,
             };
             println!("max_row_val: {}", max_row_val);
 
             // iterate through items in row
-            for current_row_value in &current_row {
+            for (current_col_index, current_row_value) in current_row.iter().enumerate() {
                 if current_row_value == max_row_val {
                     // only check cols of row element that matches max value
                     // calculate once using hashmap entry api
-                    let current_col_index = || {
-                        current_row
-                            .iter()
-                            .position(|x| x == current_row_value)
-                            .unwrap()
-                    };
-                    let cols: &mut Vec<u64> = cols.entry(row).or_insert_with(|| {
+                    let cols: &mut Vec<u64> = cols.entry(current_col_index).or_insert_with(|| {
                         let mut col: Vec<u64> = Vec::new();
                         for current_row_val in 0..row_count {
                             let new_col_val: &u64 = input
                                 .get(current_row_val)
                                 .unwrap()
-                                .get(current_col_index())
+                                .get(current_col_index)
                                 .unwrap();
                             col.push(*new_col_val);
                         }
                         col
                     });
 
-                    //
                     let min_col_val = cols.iter().min().unwrap();
+                    println!("current row: {}", row);
+                    println!("cols: {:?}", cols);
+                    println!("min_col_val: {}", min_col_val);
                     if current_row_value == min_col_val {
-                        result.push((row, current_col_index()));
+                        result.push((row, current_col_index));
                     }
                 }
             }
--- saddle-points/tests/saddle-points.rs
+++ saddle-points/tests/saddle-points.rs
@@ -37,7 +37,6 @@ fn multiple_saddle_points_in_col() {
 }
 
 #[test]
-#[ignore]
 fn multiple_saddle_points_in_row() {
     let input = vec![vec![6, 7, 8], vec![5, 5, 5], vec![7, 5, 6]];
     assert_eq!(
@@ -47,7 +46,6 @@ fn multiple_saddle_points_in_row() {
 }
 
 #[test]
-#[ignore]
 fn identify_bottom_right_saddle_point() {
     let input = vec![vec![8, 7, 9], vec![6, 7, 6], vec![3, 2, 5]];
     assert_eq!(vec![(2, 2)], find_saddle_points(&input));
@@ -55,35 +53,30 @@ fn identify_bottom_right_saddle_point() {
 
 // track specific as of v1.3
 #[test]
-#[ignore]
 fn non_square_matrix_high() {
     let input = vec![vec![1, 5], vec![3, 6], vec![2, 7], vec![3, 8]];
     assert_eq!(vec![(0, 1)], find_saddle_points(&input));
 }
 
 #[test]
-#[ignore]
 fn non_square_matrix_wide() {
     let input = vec![vec![3, 1, 3], vec![3, 2, 4]];
     assert_eq!(vec![(0, 0), (0, 2)], find_sorted_saddle_points(&input));
 }
 
 #[test]
-#[ignore]
 fn single_column_matrix() {
     let input = vec![vec![2], vec![1], vec![4], vec![1]];
     assert_eq!(vec![(1, 0), (3, 0)], find_sorted_saddle_points(&input));
 }
 
 #[test]
-#[ignore]
 fn single_row_matrix() {
     let input = vec![vec![2, 5, 3, 5]];
     assert_eq!(vec![(0, 1), (0, 3)], find_sorted_saddle_points(&input));
 }
 
 #[test]
-#[ignore]
 fn identify_all_saddle_points() {
     let input = vec![vec![5, 5, 5], vec![5, 5, 5], vec![5, 5, 5]];
     assert_eq!(
