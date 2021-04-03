2b2cbdc21d47e91e7347e47722e8ab2f650aa010
Tyler Earls <tyler.a.earls@gmail.com>
2021-04-02 22:36:45 -0500

    all tests passing

---
+++ scrabble-score/src/lib.rs
@@ -0,0 +1,18 @@
+/// Compute the Scrabble score for a word.
+pub fn score(word: &str) -> u64 {
+    let mut score: u64 = 0;
+    for c in word.chars() {
+        let points: u64 = match c.to_ascii_uppercase() {
+            'A' | 'E' | 'I' | 'O' | 'U' | 'L' | 'N' | 'R' | 'S' | 'T' => 1,
+            'D' | 'G' => 2,
+            'B' | 'C' | 'M' | 'P' => 3,
+            'F' | 'H' | 'V' | 'W' | 'Y' => 4,
+            'K' => 5,
+            'J' | 'X' => 8,
+            'Q' | 'Z' => 10,
+            _ => 0,
+        };
+        score += points;
+    }
+    score
+}
