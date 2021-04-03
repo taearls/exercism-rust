a3d70d8efc0627e833db157914be93e596346b31
Tyler Earls <tyler.a.earls@gmail.com>
2021-04-02 22:10:44 -0500

    get 1st test passing

--- hamming/src/lib.rs
+++ hamming/src/lib.rs
@@ -1,5 +1,20 @@
 /// Return the Hamming distance between the strings,
 /// or None if the lengths are mismatched.
 pub fn hamming_distance(s1: &str, s2: &str) -> Option<usize> {
-    unimplemented!("What is the Hamming Distance between {} and {}", s1, s2);
+    if s1.len() != s2.len() {
+        None
+    } else {
+        match s1.len() {
+            0 => Some(0),
+            _ => {
+                let mut distance: usize = 0;
+                for (i, c) in s1.char_indices() {
+                    if c != s2.chars().nth(i).unwrap() {
+                        distance += 1;
+                    }
+                }
+                Some(distance)
+            }
+        }
+    }
 }
