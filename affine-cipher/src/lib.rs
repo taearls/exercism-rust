831d8d1c6920e3426fd2f3d4542f4615f3e8577b
Tyler Earls <tyler.a.earls@gmail.com>
2021-05-16 15:10:13 -0500

    get 1st test passing

--- affine-cipher/src/lib.rs
+++ affine-cipher/src/lib.rs
@@ -1,18 +1,22 @@
-/// While the problem description indicates a return status of 1 should be returned on errors,
-/// it is much more common to return a `Result`, so we provide an error type for the result here.
 #[derive(Debug, Eq, PartialEq)]
 pub enum AffineCipherError {
     NotCoprime(i32),
 }
 
-/// Encodes the plaintext using the affine cipher with key (`a`, `b`). Note that, rather than
-/// returning a return code, the more common convention in Rust is to return a `Result`.
 pub fn encode(plaintext: &str, a: i32, b: i32) -> Result<String, AffineCipherError> {
-    unimplemented!("Encode {} with the key ({}, {})", plaintext, a, b);
+    let mut str = String::with_capacity(plaintext.len());
+    for c in plaintext.chars() {
+        let new_char = ((a * letter_index(c) + b) % 26) as u8 + 97;
+        str.push(new_char as char);
+    }
+    Ok(str)
 }
 
-/// Decodes the ciphertext using the affine cipher with key (`a`, `b`). Note that, rather than
-/// returning a return code, the more common convention in Rust is to return a `Result`.
 pub fn decode(ciphertext: &str, a: i32, b: i32) -> Result<String, AffineCipherError> {
     unimplemented!("Decode {} with the key ({}, {})", ciphertext, a, b);
+}
+
+fn letter_index(c: char) -> i32 {
+    // 'a' has a value of 97
+    c.to_ascii_lowercase() as i32 - 97
 }
