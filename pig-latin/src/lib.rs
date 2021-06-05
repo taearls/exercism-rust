cc1b491c6e5beff245525f7e7010c0dcab6212dc
Tyler Earls <tyler.a.earls@gmail.com>
2021-06-05 17:14:36 -0500

    get more tests passing

--- pig-latin/src/lib.rs
+++ pig-latin/src/lib.rs
@@ -2,7 +2,11 @@ pub fn translate(input: &str) -> String {
     let mut result = String::from(input);
     match input.chars().nth(0) {
         Some('A' | 'a' | 'e' | 'E' | 'i' | 'I' | 'o' | 'O' | 'u' | 'U') => result.push_str("ay"),
-        _ => ()
+        Some(c) => {
+            result.remove(0);
+            result.push_str(&format!("{}ay", c));
+        },
+        None => (),
     }
     result
 }
