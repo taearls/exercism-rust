14dd89b8e903e365f8c53d4ca33290ce573082ba
Tyler Earls <tyler.a.earls@gmail.com>
2021-06-05 20:49:35 -0500

    get all tests passing

--- pig-latin/src/lib.rs
+++ pig-latin/src/lib.rs
@@ -2,22 +2,28 @@ use lazy_static::lazy_static;
 use regex::Regex;
 
 pub fn translate(input: &str) -> String {
-    let mut result = String::from(input);
-    match input.chars().next() {
-        Some(c) if matches_vowels(&c.to_string()) => result.push_str("ay"),
-        Some(c) => {
-            if let Some(found) = matches_consonant(input) {
-                result.replace_range(..found[0].len(), "");
-                result.push_str(&found[0]);
-            } else if !chars_to_be_ignored(&input[0..2]) {
-                result.remove(0);
-                result.push(c);
-            } 
-            result.push_str("ay");
-        },
-        None => (),
+    let mut result: Vec<String> = Vec::new();
+    let words: Vec<&str> = input.split(' ').collect();
+
+    for word in words.iter() {
+        let mut temp_str = word.to_string();
+        match word.chars().next() {
+            Some(c) if matches_vowels(&c.to_string()) => temp_str.push_str("ay"),
+            Some(c) => {
+                if let Some(found) = matches_consonant(word) {
+                    temp_str.replace_range(..found[0].len(), "");
+                    temp_str.push_str(&found[0]);
+                } else if !chars_to_be_ignored(&word[0..2]) {
+                    temp_str.remove(0);
+                    temp_str.push(c);
+                } 
+                temp_str.push_str("ay");
+            },
+            None => (),
+        }
+        result.push(temp_str);
     }
-    result
+    result.join(" ")
 }
 
 fn matches_consonant(input: &str) -> Option<regex::Captures<'_>> {
