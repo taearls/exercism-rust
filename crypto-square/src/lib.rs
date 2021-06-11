c47c37c046a357f52fbeafac4bfd499e45347357
Tyler Earls <tyler.a.earls@gmail.com>
2021-06-10 23:34:04 -0500

    mix and match ideas from community solutions, clean up code

--- crypto-square/src/lib.rs
+++ crypto-square/src/lib.rs
@@ -1,34 +1,27 @@
 pub fn encrypt(input: &str) -> String {
-    let (rows, row_length) = get_chunk_length(input);
-    let mut result = String::with_capacity(rows * row_length);
+    let (rows, cols) = get_str_rect(input);
 
     let filtered_str = input
+        .to_lowercase()
         .chars()
         .filter(|c| c.is_alphanumeric())
-        .map(|c| c.to_lowercase().to_string())
         .collect::<String>();
 
-    let padded_str = format!("{:width$}", filtered_str, width = rows * row_length);
+    // create vec with empty strings of correct length
+    let mut str_vec = (0..cols)
+        .map(|_| String::with_capacity(cols))
+        .collect::<Vec<_>>();
+        
+    // create padded string so we account for extra spaces
+    let padded_str = format!("{:width$}", filtered_str, width = rows * cols);
 
-    let mut str_vec: Vec<&str> = Vec::new();
-    for row in 0..rows {
-        let index = row * row_length;
-        str_vec.push(padded_str.get(index..index + row_length).unwrap())
+    for (i, c) in padded_str.chars().enumerate() {
+        str_vec[i % cols].push(c);
     }
-
-    for col in 0..row_length {
-        let mut index = col;
-        while index < padded_str.len() {
-            result.push_str(padded_str.get(index..index + 1).unwrap());
-            index += row_length;
-        }
-        if col < row_length - 1 {
-            result.push(' ');
-        }
-    }
-    result
+    str_vec.join(" ")
 }
-fn get_chunk_length(input: &str) -> (usize, usize) {
+
+fn get_str_rect(input: &str) -> (usize, usize) {
     let trimmed_str: String = input.split_whitespace().collect();
     let sqrt = (trimmed_str.len() as f32).sqrt();
     (sqrt.floor() as usize, sqrt.ceil() as usize)
