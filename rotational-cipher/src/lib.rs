2073cd9b094da019daff623dcbdab4778bd5fb4c
Tyler Earls <tyler.a.earls@gmail.com>
2021-06-29 23:52:53 -0500

    get first test passing

--- rotational-cipher/src/lib.rs
+++ rotational-cipher/src/lib.rs
@@ -1,7 +1,13 @@
+const ALPHABET: [char; 26] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
 pub fn rotate(input: &str, key: i8) -> String {
-    unimplemented!(
-        "How would input text '{}' transform when every letter is shifted using key '{}'?",
-        input,
-        key
-    );
+    input
+        .to_ascii_lowercase()
+        .chars()
+        .map(|c| {
+            let old_val = ALPHABET.iter().position(|&x| x == c).unwrap();
+            let new_val = (old_val + (key as usize)) % 26;
+            return ALPHABET[new_val];
+        })
+        .collect()
 }
