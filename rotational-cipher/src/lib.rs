0f1d02c4bc2ae7a343da94bad8f17ce660221db6
Tyler Earls <tyler.a.earls@gmail.com>
2021-06-30 00:13:16 -0500

    get all tests passing

--- rotational-cipher/src/lib.rs
+++ rotational-cipher/src/lib.rs
@@ -1,13 +1,32 @@
-const ALPHABET: [char; 26] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const ALPHABET: [char; 26] = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y', 'z',
+];
 
 pub fn rotate(input: &str, key: i8) -> String {
     input
-        .to_ascii_lowercase()
         .chars()
         .map(|c| {
-            let old_val = ALPHABET.iter().position(|&x| x == c).unwrap();
-            let new_val = (old_val + (key as usize)) % 26;
-            return ALPHABET[new_val];
+            match ALPHABET
+                .iter()
+                .position(|&x| x.to_ascii_lowercase() == c.to_ascii_lowercase())
+            {
+                Some(x) => {
+                    let mut index = ((x as i8) + key) % 26;
+
+                    // if index is negative because of a negative key, keep adding 26 until it's positive.
+                    while index < 0 {
+                        index += 26;
+                    }
+
+                    if c.is_ascii_uppercase() {
+                        ALPHABET[index as usize].to_ascii_uppercase()
+                    } else {
+                        ALPHABET[index as usize]
+                    }
+                }
+                None => c,
+            }
         })
         .collect()
 }
