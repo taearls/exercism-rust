9ab5a6f973fc85165067c671e4b29f64482c65ce
Tyler Earls <tyler.a.earls@gmail.com>
2021-07-04 10:59:59 -0500

    get first test passing

--- simple-cipher/src/lib.rs
+++ simple-cipher/src/lib.rs
@@ -1,5 +1,34 @@
+const ALPHABET: [char; 26] = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y', 'z',
+];
+
 pub fn encode(key: &str, s: &str) -> Option<String> {
-    unimplemented!("Use {} to encode {} using shift cipher", key, s)
+    if key.len() != s.len()
+        || s.chars().filter(|c| !c.is_ascii_lowercase()).count() > 0
+        || key.chars().filter(|c| !c.is_ascii_lowercase()).count() > 0
+    {
+        None
+    } else {
+        Some(
+            s.char_indices()
+                .map(|(i, c)| {
+                    let alphabet_pos = ALPHABET.iter().position(|&x| x == c).unwrap();
+                    let key_at_pos = key.get(i..i + 1).unwrap();
+                    let key_pos = ALPHABET
+                        .iter()
+                        .position(|&x| x.to_string() == key_at_pos)
+                        .unwrap();
+                    let new_index = (alphabet_pos + key_pos) % 26;
+                    let new_char = ALPHABET.get(new_index..new_index + 1).unwrap();
+                    (i, new_char[0])
+                })
+                .fold(String::new(), |acc, (_i, c)| {
+                    let mut buf = [0; 2];
+                    acc + c.encode_utf8(&mut buf)
+                }),
+        )
+    }
 }
 
 pub fn decode(key: &str, s: &str) -> Option<String> {
