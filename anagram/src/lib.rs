16e11f4c0e3ddd0303bd49c4d20961144507c79d
Tyler Earls <tyler.a.earls@gmail.com>
2021-07-05 22:20:45 -0500

    cargo fmt

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -1,21 +1,23 @@
 use std::collections::HashSet;
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&str]) -> HashSet<&'a str> {
-    possible_anagrams.iter().filter_map(|candidate| {
-        let mut temp = String::from(word);
-        for c in candidate.chars() {
-            if let Some(pos) = temp.chars().position(|x| x == c) {
-                temp.replace_range(pos..pos + 1, "");
-            } else {
-                return None;
+    possible_anagrams
+        .iter()
+        .filter_map(|candidate| {
+            let mut temp = String::from(word);
+            for c in candidate.chars() {
+                if let Some(pos) = temp.chars().position(|x| x == c) {
+                    temp.replace_range(pos..pos + 1, "");
+                } else {
+                    return None;
+                }
             }
-        }
-        
-        if !temp.is_empty() {
-            None
-        } else {
-            Some(*candidate)
-        }
-    })
-    .collect::<HashSet<&'a str>>()
+
+            if !temp.is_empty() {
+                None
+            } else {
+                Some(*candidate)
+            }
+        })
+        .collect::<HashSet<&'a str>>()
 }
