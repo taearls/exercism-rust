8f254dd9a2dc40586696e6d5ea04c868f3d579d6
Tyler Earls <tyler.a.earls@gmail.com>
2021-08-03 19:49:33 -0500

    get all tests passing

--- robot-name/src/lib.rs
+++ robot-name/src/lib.rs
@@ -1,15 +1,45 @@
-pub struct Robot;
+const ROBOT_NAME_LEN: usize = 5;
+
+pub struct Robot {
+    name: String,
+}
 
 impl Robot {
     pub fn new() -> Self {
-        unimplemented!("Construct a new Robot struct.");
+        Robot {
+            name: gen_robot_name(""),
+        }
     }
 
     pub fn name(&self) -> &str {
-        unimplemented!("Return the reference to the robot's name.");
+        &self.name
     }
 
     pub fn reset_name(&mut self) {
-        unimplemented!("Assign a new unique name to the robot.");
+        self.name = gen_robot_name(&self.name);
     }
+}
+
+fn gen_robot_name(old_name: &str) -> String {
+    let mut name = String::with_capacity(ROBOT_NAME_LEN);
+
+    for i in 0..ROBOT_NAME_LEN {
+        let modulo: u8;
+        let ascii_offset: u8;
+
+        if i < 2 {
+            modulo = 26;
+            ascii_offset = 65; // 65 = 'A'
+        } else {
+            modulo = 10;
+            ascii_offset = 48; // 48 = '0'
+        }
+        let c = (rand::random::<u8>() % modulo + ascii_offset) as char;
+        name.push(c);
+    }
+
+    if name == old_name {
+        name = gen_robot_name(old_name);
+    }
+    name
 }
