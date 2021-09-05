76689ed2810fc735f2830ea7b5891911c7f43c31
Tyler Earls <tyler.a.earls@gmail.com>
2021-09-05 14:55:29 -0500

    get first test passing

--- rail-fence-cipher/src/lib.rs
+++ rail-fence-cipher/src/lib.rs
@@ -1,12 +1,43 @@
-pub struct RailFence;
+pub struct RailFence {
+    rails: usize,
+}
 
 impl RailFence {
     pub fn new(rails: u32) -> RailFence {
-        unimplemented!("Construct a new fence with {} rails", rails)
+        RailFence { rails: rails as usize }
     }
 
     pub fn encode(&self, text: &str) -> String {
-        unimplemented!("Encode this text: {}", text)
+        let mut result = String::with_capacity(text.len());
+        let mut vec: Vec<String> = Vec::with_capacity(self.rails);
+
+        // initialize vec with strings
+        for _ in 0..self.rails {
+            vec.push(String::with_capacity(text.len()));
+        }
+
+        // build up rail cypher
+        for pos in 0..text.len() {
+            let row_with_char = pos % self.rails;
+            for row in 0..self.rails {
+                if row == row_with_char {
+                    vec[row].push(text.chars().nth(pos).unwrap());
+                } else {
+                    vec[row].push('.');
+                }
+            }
+        }
+
+        // get encoded str in rail cypher
+        for rail in 0..self.rails {
+            for c in vec[rail].chars() {
+                if c != '.' {
+                    result.push(c);
+                }
+            } 
+        }
+
+        result
     }
 
     pub fn decode(&self, cipher: &str) -> String {
