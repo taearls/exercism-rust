65b92db0f4b28faa5d6b538a2547f2ce5086887c
Tyler Earls <tyler.a.earls@gmail.com>
2021-09-10 20:59:41 -0500

    refactor decode fn

--- rail-fence-cipher/src/lib.rs
+++ rail-fence-cipher/src/lib.rs
@@ -12,12 +12,13 @@ impl RailFence {
     }
 
     pub fn encode(&self, text: &str) -> String {
+        let graphemes: Vec<&str> = text.graphemes(true).collect();
+
         let mut vec: Vec<String> = (0..self.rails)
-            .map(|_| String::with_capacity(text.len()))
+            .map(|_| String::with_capacity(graphemes.len()))
             .collect();
 
         // build up rail cypher
-        let graphemes: Vec<&str> = text.graphemes(true).collect();
         for pos in 0..graphemes.len() {
             let row_with_char = get_row(pos, self.rails);
             for (row, item) in vec.iter_mut().enumerate().take(self.rails) {
@@ -34,8 +35,27 @@ impl RailFence {
             .collect::<String>()
     }
 
+    // T . . . E . . . I . . . T . . . E . . . L .
+    // . H . D . V . L . S . N . H . D . T . I . S
+    // . . E . . . I . . . I . . . E . . . A . . .
+
     pub fn decode(&self, cipher: &str) -> String {
-        unimplemented!("Decode this ciphertext: {}", cipher)
+        // let graphemes: Vec<&str> = cipher.graphemes(true).collect();
+        // let vec: Vec<String> = (0..self.rails)
+        //     .map(|rail| build_row(self.rails, rail, cipher))
+        //     .collect();
+        // println!("vec: {:?}", vec);
+
+        // cipher
+        //     .chars()
+        //     .map(|c| {
+
+        //     });
+
+        // vec.iter()
+        //     .map(|row| row.chars().filter(|c| *c != '.').collect::<String>())
+        //     .collect::<String>()
+        String::from(cipher)
     }
 }
 
@@ -57,3 +77,18 @@ fn get_row(pos: usize, rail_count: usize) -> usize {
     let idx = pos % (rail_count + rail_count - 2);
     vec[idx]
 }
+
+// fn build_row(total_rows: usize, current_row: usize, cipher: &str) -> String {
+//     let period_offset = current_row % total_rows;
+//     let period_filler_count = if current_row == 0 || current_row == total_rows - 1 {
+//         total_rows
+//     } else {
+//         total_rows - current_row
+//     };
+//     format!(
+//         "{}{}{}",
+//         ".".repeat(period_offset),
+//         cipher.to_string().drain(0..1).collect::<String>(),
+//         ".".repeat(period_filler_count)
+//     )
+// }
