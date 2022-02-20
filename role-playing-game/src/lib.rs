a405973f31da63ef265412377056dcd61d3179ac
Tyler Earls <tyler.a.earls@gmail.com>
2022-02-20 12:13:34 -0600

    get all tests passing

--- role-playing-game/src/lib.rs
+++ role-playing-game/src/lib.rs
@@ -10,10 +10,34 @@ pub struct Player {
 
 impl Player {
     pub fn revive(&self) -> Option<Player> {
-        unimplemented!("Revive this player")
+        if self.health == 0 {
+            let mut new_mana = self.mana;
+            if self.level >= 10 {
+                new_mana = Some(100);
+            }
+            Some(Player {
+                health: 100,
+                mana: new_mana,
+                level: self.level,
+            })
+        } else {
+            None
+        }
     }
 
     pub fn cast_spell(&mut self, mana_cost: u32) -> u32 {
-        unimplemented!("Cast a spell of cost {}", mana_cost)
+        let mut mana_spent = 0;
+        if self.mana.is_none() {
+            self.health -= if self.health > mana_cost {
+                mana_cost
+            } else {
+                self.health
+            };
+        } else if self.mana.unwrap() > 0 && mana_cost < self.mana.unwrap() {
+            let total_damage = mana_cost * 2;
+            self.mana = Some(self.mana.unwrap() - mana_cost);
+            mana_spent = total_damage;
+        }
+        mana_spent
     }
 }
