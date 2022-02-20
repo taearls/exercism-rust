d79f8c26a6b4e59d29628b3e08cfef3ee06f8204
Tyler Earls <tyler.a.earls@gmail.com>
2022-02-19 18:35:02 -0600

    get all tests passing

--- resistor-color/src/lib.rs
+++ resistor-color/src/lib.rs
@@ -13,16 +13,40 @@ pub enum ResistorColor {
 }
 
 pub fn color_to_value(_color: ResistorColor) -> usize {
-    unimplemented!("convert a color into a numerical representation")
+    use ResistorColor::*;
+    match _color {
+        Black => 0,
+        Brown => 1,
+        Red => 2,
+        Orange => 3,
+        Yellow => 4,
+        Green => 5,
+        Blue => 6,
+        Violet => 7,
+        Grey => 8,
+        White => 9,
+    }
 }
 
 pub fn value_to_color_string(value: usize) -> String {
-    unimplemented!(
-        "convert the value {} into a string representation of color",
-        value
-    )
+    match value {
+        0 => String::from("Black"),
+        1 => String::from("Brown"),
+        2 => String::from("Red"),
+        3 => String::from("Orange"),
+        4 => String::from("Yellow"),
+        5 => String::from("Green"),
+        6 => String::from("Blue"),
+        7 => String::from("Violet"),
+        8 => String::from("Grey"),
+        9 => String::from("White"),
+        _ => String::from("value out of range"),
+    }
 }
 
 pub fn colors() -> Vec<ResistorColor> {
-    unimplemented!("return a list of all the colors ordered by resistance")
+    use ResistorColor::*;
+    vec![
+        Black, Brown, Red, Orange, Yellow, Green, Blue, Violet, Grey, White,
+    ]
 }
