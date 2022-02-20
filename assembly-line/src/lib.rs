d1279820dc4805259471f251f20704c0bb2b04ba
Tyler Earls <tyler.a.earls@gmail.com>
2022-02-19 18:12:05 -0600

    get all tests passing

--- assembly-line/src/lib.rs
+++ assembly-line/src/lib.rs
@@ -1,11 +1,15 @@
-// This stub file contains items which aren't used yet; feel free to remove this module attribute
-// to enable stricter warnings.
-#![allow(unused)]
-
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    unimplemented!("calculate hourly production rate at speed: {}", speed)
+    let base_hourly_rate: f64 = 221. * (speed as f64);
+    let success_rate: f64 = match speed {
+        0 => 0.,
+        1..=4 => 1.,
+        5..=8 => 0.9,
+        9..=10 => 0.77,
+        _ => panic!("Invalid speed provided: {}", speed),
+    };
+    base_hourly_rate * success_rate
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    unimplemented!("calculate the amount of working items at speed: {}", speed)
+    (production_rate_per_hour(speed) / 60.) as u32
 }
