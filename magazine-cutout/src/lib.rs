0a29a2c9dbe5b3c0733e10283ceb360cda548d09
Tyler Earls <tyler.a.earls@gmail.com>
2022-02-20 13:13:04 -0600

    get all tests passing

--- magazine-cutout/src/lib.rs
+++ magazine-cutout/src/lib.rs
@@ -1,9 +1,17 @@
-// This stub file contains items which aren't used yet; feel free to remove this module attribute
-// to enable stricter warnings.
-#![allow(unused)]
-
 use std::collections::HashMap;
 
 pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
-    unimplemented!()
+    let mut hm: HashMap<&str, usize> = HashMap::new();
+
+    for word in magazine.iter() {
+        *hm.entry(word).or_insert(0) += 1;
+    }
+    for word in note.iter() {
+        if hm.contains_key(word) && *hm.get(word).unwrap() > 0 {
+            *hm.get_mut(word).unwrap() -= 1;
+        } else {
+            return false;
+        }
+    }
+    true
 }
