2a68bfbb47d0b2ca9dc050336e2fea7a66c17db3
Tyler Earls <tyler.a.earls@gmail.com>
2022-02-19 18:20:41 -0600

    get all tests passing

--- semi-structured-logs/src/lib.rs
+++ semi-structured-logs/src/lib.rs
@@ -1,24 +1,35 @@
-// This stub file contains items which aren't used yet; feel free to remove this module attribute
-// to enable stricter warnings.
-#![allow(unused)]
+use std::fmt;
 
-/// various log levels
 #[derive(Clone, PartialEq, Debug)]
 pub enum LogLevel {
     Info,
     Warning,
     Error,
+    Debug,
+}
+impl fmt::Display for LogLevel {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            LogLevel::Info => write!(f, "INFO"),
+            LogLevel::Warning => write!(f, "WARNING"),
+            LogLevel::Error => write!(f, "ERROR"),
+            LogLevel::Debug => write!(f, "DEBUG"),
+        }
+    }
 }
 /// primary function for emitting logs
 pub fn log(level: LogLevel, message: &str) -> String {
-    unimplemented!()
+    format!("[{level}]: {message}")
 }
 pub fn info(message: &str) -> String {
-    unimplemented!()
+    log(LogLevel::Info, message)
 }
 pub fn warn(message: &str) -> String {
-    unimplemented!()
+    log(LogLevel::Warning, message)
 }
 pub fn error(message: &str) -> String {
-    unimplemented!()
+    log(LogLevel::Error, message)
+}
+pub fn debug(message: &str) -> String {
+    log(LogLevel::Debug, message)
 }
