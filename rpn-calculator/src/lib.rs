7ac300fac4067fb93aa8bea6a73116a4e7c54b31
Tyler Earls <tyler.a.earls@gmail.com>
2022-02-21 17:55:35 -0600

    get all tests passing

--- rpn-calculator/src/lib.rs
+++ rpn-calculator/src/lib.rs
@@ -1,4 +1,4 @@
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum CalculatorInput {
     Add,
     Subtract,
@@ -8,8 +8,40 @@ pub enum CalculatorInput {
 }
 
 pub fn evaluate(inputs: &[CalculatorInput]) -> Option<i32> {
-    unimplemented!(
-		"Given the inputs: {:?}, evaluate them as though they were a Reverse Polish notation expression",
-		inputs,
-	);
+    let mut values: Vec<i32> = vec![];
+    for input in inputs.iter() {
+        match input {
+            CalculatorInput::Value(value) => values.push(*value),
+            _ if values.len() < 2 => return None,
+            CalculatorInput::Add => {
+                let (x, y) = get_last_two_values_in_order(&mut values);
+                values.push(x + y);
+            }
+            CalculatorInput::Subtract => {
+                let (x, y) = get_last_two_values_in_order(&mut values);
+                values.push(x - y);
+            }
+            CalculatorInput::Multiply => {
+                let (x, y) = get_last_two_values_in_order(&mut values);
+                values.push(x * y);
+            }
+            CalculatorInput::Divide => {
+                let (x, y) = get_last_two_values_in_order(&mut values);
+                values.push(x / y);
+            }
+        }
+    }
+
+    if values.len() == 1 {
+        Some(values[0])
+    } else {
+        None
+    }
+}
+
+fn get_last_two_values_in_order(values: &mut Vec<i32>) -> (i32, i32) {
+    (
+        values.remove(values.len() - 2),
+        values.remove(values.len() - 1),
+    )
 }
