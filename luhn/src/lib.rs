070fef077c90dcaca31f69e012067525f90d1f1d
Tyler Earls <tyler.a.earls@gmail.com>
2022-02-23 00:31:17 -0600

    get almost all tests passing

--- luhn/src/lib.rs
+++ luhn/src/lib.rs
@@ -1,4 +1,35 @@
-/// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-    unimplemented!("Is the Luhn checksum for {} valid?", code);
+    let filtered_digits = code
+        .chars()
+        .filter_map(|c| c.to_digit(10))
+        .collect::<Vec<u32>>();
+    if filtered_digits.len() < 2
+        || filtered_digits.len() != code.chars().filter(|c| *c != ' ').count()
+    {
+        return false;
+    }
+
+    filtered_digits
+        .iter()
+        .enumerate()
+        .map(|(index, &digit)| {
+            let mut digit = digit;
+            if code.len() % 2 == 0 {
+                if index % 2 == 0 {
+                    digit *= 2;
+                    if digit > 9 {
+                        digit -= 9;
+                    }
+                }
+            } else if index % 2 == 1 {
+                digit *= 2;
+                if digit > 9 {
+                    digit -= 9;
+                }
+            }
+            digit
+        })
+        .sum::<u32>()
+        % 10
+        == 0
 }
