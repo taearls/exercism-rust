9cf6bc653ccd51bcdfd72163300e8c5c9de66aeb
Tyler Earls <tyler.a.earls@gmail.com>
2022-02-27 01:10:55 -0600

    get all tests passing

--- phone-number/src/lib.rs
+++ phone-number/src/lib.rs
@@ -1,6 +1,35 @@
 pub fn number(user_number: &str) -> Option<String> {
-    unimplemented!(
-        "Given the number entered by user '{}', convert it into SMS-friendly format. If the entered number is not a valid NANP number, return None.",
-        user_number
-    );
+    let mut digits = user_number
+        .chars()
+        .filter(|c| c.is_numeric())
+        .collect::<String>();
+
+    if digits.len() == 11 {
+        if digits.get(0..1).unwrap() != "1" {
+            return None;
+        } else {
+            digits.drain(0..1);
+        }
+    }
+
+    if digits.len() != 10 {
+        return None;
+    }
+
+    let mut is_valid = true;
+    let result = digits
+        .chars()
+        .enumerate()
+        .map(|(index, digit)| {
+            if [0, 3].contains(&index) && ['0', '1'].contains(&digit) {
+                is_valid = false;
+            }
+            digit
+        })
+        .collect::<String>();
+    if is_valid {
+        Some(result)
+    } else {
+        None
+    }
 }
