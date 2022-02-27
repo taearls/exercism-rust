f519b071af613163afc3cfb823149d65be6eb866
Tyler Earls <tyler.a.earls@gmail.com>
2022-02-27 13:13:50 -0600

    get all tests passing

--- largest-series-product/src/lib.rs
+++ largest-series-product/src/lib.rs
@@ -5,9 +5,31 @@ pub enum Error {
 }
 
 pub fn lsp(string_digits: &str, span: usize) -> Result<u64, Error> {
-    unimplemented!(
-        "largest series product of a span of {} digits in {}",
-        span,
-        string_digits
-    );
+    if span > string_digits.len() {
+        return Err(Error::SpanTooLong);
+    } else if string_digits.is_empty() || span == 0 {
+        return Ok(1);
+    }
+
+    let invalid_chars: Vec<char> = string_digits.chars().filter(|c| !c.is_numeric()).collect();
+    if !invalid_chars.is_empty() {
+        let invalid_char = invalid_chars.get(0).unwrap();
+        return Err(Error::InvalidDigit(*invalid_char));
+    }
+
+    let mut product: u64 = 0;
+
+    let digits = string_digits
+        .chars()
+        .map(|c| c.to_digit(10).unwrap() as u64)
+        .collect::<Vec<u64>>();
+
+    for slice in digits.windows(span) {
+        let slice_product: u64 = slice.iter().product();
+        if slice_product > product {
+            product = slice_product;
+        }
+    }
+
+    Ok(product)
 }
