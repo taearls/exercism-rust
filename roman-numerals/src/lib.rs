83aa154a631d9f47c4db2918468dd35b34d776de
Tyler Earls <tyler.a.earls@gmail.com>
2022-03-18 01:11:38 -0500

    work in progress

--- roman-numerals/src/lib.rs
+++ roman-numerals/src/lib.rs
@@ -1,10 +1,26 @@
+use lazy_static::lazy_static;
+use std::collections::HashMap;
 use std::fmt::{Display, Formatter, Result};
 
-pub struct Roman;
+lazy_static! {
+    static ref HASHMAP: HashMap<u32, &'static str> = {
+        HashMap::from([
+            (1, "I"),
+            (5, "V"),
+            (10, "X"),
+            (50, "L"),
+            (100, "C"),
+            (500, "D"),
+            (1000, "M"),
+        ])
+    };
+}
+
+pub struct Roman { num: String }
 
 impl Display for Roman {
-    fn fmt(&self, _f: &mut Formatter<'_>) -> Result {
-        unimplemented!("Return a roman-numeral string representation of the Roman object");
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        write!(f, "{}", self.num)
     }
 }
 
