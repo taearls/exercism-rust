2a2aa187c0a73a620654aabd9468a92f50c8e7a7
Tyler Earls <tyler.a.earls@gmail.com>
2022-03-19 16:17:34 -0500

    get simple test case passing

--- roman-numerals/src/lib.rs
+++ roman-numerals/src/lib.rs
@@ -1,10 +1,10 @@
 use lazy_static::lazy_static;
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 use std::fmt::{Display, Formatter, Result};
 
 lazy_static! {
-    static ref HASHMAP: HashMap<u32, &'static str> = {
-        HashMap::from([
+    static ref HASHMAP: BTreeMap<u32, &'static str> = {
+        BTreeMap::from([
             (1, "I"),
             (5, "V"),
             (10, "X"),
@@ -16,16 +16,25 @@ lazy_static! {
     };
 }
 
-pub struct Roman { num: String }
+pub struct Roman(String);
 
 impl Display for Roman {
     fn fmt(&self, f: &mut Formatter) -> Result {
-        write!(f, "{}", self.num)
+        write!(f, "{}", self.0)
     }
 }
 
 impl From<u32> for Roman {
     fn from(num: u32) -> Self {
-        unimplemented!("Construct a Roman object from the '{}' number", num);
+        let mut result = String::new();
+        let mut num = num;
+        for (key, value) in HASHMAP.iter().rev() {
+            println!("key is: {key}, value is {value}");
+            while &num >= key {
+                result.push_str(value);
+                num -= key;
+            }
+        }
+        Self(result)
     }
 }
