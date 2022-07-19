4fd43ce167be2915c0456b2e0e79d1b0bfd81fde
Tyler Earls <tyler.a.earls@gmail.com>
2022-07-19 08:13:57 -0500

    create initial implementation without chromatic scales in mind

--- scale-generator/src/lib.rs
+++ scale-generator/src/lib.rs
@@ -1,3 +1,6 @@
+use lazy_static::lazy_static;
+use regex::Regex;
+
 // You should change this.
 //
 // Depending on your implementation, there are a variety of potential errors
@@ -10,24 +13,70 @@
 // errors. Another common idiom is to use a helper type such as failure::Error
 // which does more or less the same thing but automatically.
 #[derive(Debug)]
-pub struct Error;
+pub enum Error {
+    InvalidTonic,
+    InvalidIntervals,
+}
 
-pub struct Scale;
+pub struct Scale {
+    tonic: String,
+    intervals: String,
+}
+
+fn is_valid_intervals_str(intervals_str: &str) -> bool {
+    lazy_static! {
+        static ref INTERVAL_REGEX: Regex = Regex::new(r"^[mMaA]*$").unwrap();
+    }
+    INTERVAL_REGEX.is_match(intervals_str)
+}
+
+fn is_valid_note_str(note_str: &str) -> bool {
+    lazy_static! {
+      // check if str has a-g or A-G in one occurrence
+      // check for one flat, or one sharp
+      static ref NOTE_REGEX: Regex = Regex::new(
+        r"^(?P<note_name>(?i)[a-g]{1})(?P<note_variant>(?-i)(b{1})|(#{1}))?$"
+      ).unwrap();
+    }
+    NOTE_REGEX.is_match(note_str)
+}
 
 impl Scale {
     pub fn new(tonic: &str, intervals: &str) -> Result<Scale, Error> {
-        unimplemented!(
-            "Construct a new scale with tonic {} and intervals {}",
-            tonic,
-            intervals
-        )
+        if !is_valid_note_str(tonic) {
+            Err(Error::InvalidTonic)
+        } else if !is_valid_intervals_str(intervals) {
+            Err(Error::InvalidIntervals)
+        } else {
+            Ok(Scale {
+                tonic: tonic.to_string(),
+                intervals: intervals.to_string(),
+            })
+        }
     }
 
     pub fn chromatic(tonic: &str) -> Result<Scale, Error> {
-        unimplemented!("Construct a new chromatic scale with tonic {}", tonic)
+        if !is_valid_note_str(tonic) {
+            Err(Error::InvalidTonic)
+        } else {
+            Ok(Scale {
+                tonic: tonic.to_string(),
+                intervals: "m".repeat(11).to_string(),
+            })
+        }
     }
 
     pub fn enumerate(&self) -> Vec<String> {
-        unimplemented!()
+        let mut notes = vec![self.tonic.clone()];
+        for interval in self.intervals.chars() {
+            match interval {
+                'm' => notes.push(notes.last().unwrap().clone()),
+                'M' => notes.push(notes.last().unwrap().clone()),
+                'A' => notes.push(notes.last().unwrap().clone()),
+                'a' => notes.push(notes.last().unwrap().clone()),
+                _ => panic!("Invalid interval"),
+            }
+        }
+        notes
     }
 }
