ba5f6468ab332c1c62019b204fda28c553798033
Tyler Earls <tyler.a.earls@gmail.com>
2022-07-19 11:24:24 -0500

    update collatz-conjecture solution so tests pass

--- collatz-conjecture/src/lib.rs
+++ collatz-conjecture/src/lib.rs
@@ -1,6 +1,6 @@
 pub fn collatz(n: u64) -> Option<u64> {
     if n < 1 {
-        return None
+        return None;
     }
 
     let mut steps = 0;
@@ -9,6 +9,9 @@ pub fn collatz(n: u64) -> Option<u64> {
         if temp_n == 1 {
             break;
         } else if temp_n % 2 == 1 {
+            if temp_n.checked_mul(3).is_none() || (temp_n * 3).checked_add(1).is_none() {
+                return None;
+            }
             temp_n = (3 * temp_n) + 1;
         } else {
             temp_n /= 2;
