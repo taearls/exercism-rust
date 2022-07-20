5238d23fa2cff61edc31a86cbc008f99ff7948b2
Tyler Earls <tyler.a.earls@gmail.com>
2022-07-20 00:59:05 -0500

    get solution working with nightly build

--- palindrome-products/src/lib.rs
+++ palindrome-products/src/lib.rs
@@ -15,8 +15,6 @@ impl Palindrome {
     pub fn into_inner(&self) -> u64 {
         self.inner
     }
-
-    // pub fn insert(&mut self, (_from, _to): (u64, u64)) {}
 }
 
 fn is_palindrome(num: u64) -> bool {
@@ -29,37 +27,42 @@ pub fn palindrome_products(min: u64, max: u64) -> Option<(Palindrome, Palindrome
     if min > max {
         return None;
     }
-    let mut result: Option<(Palindrome, Palindrome)> = None;
-    let should_merge_sort = (max - min) / 2 >= min;
+    let mut min_product: Option<u64> = None;
+    let mut max_product: Option<u64> = None;
 
-    let min_products_max_range = if should_merge_sort { (max - min) / 2 } else { max };
-    let min_products = find_palindrome_products_in_range(min, min_products_max_range);
-    if !min_products.is_empty() {
-        let min_palindrome = Palindrome::new(*min_products.iter().min().unwrap()).unwrap();
-        let max_palindrome = Palindrome::new(*min_products.iter().max().unwrap()).unwrap();
-        result = Some((min_palindrome, max_palindrome));
-    }
-
-    if should_merge_sort {
-        let max_products = find_palindrome_products_in_range(max / 2, max);
-        if !max_products.is_empty() {
-            let min_product = if min_products.is_empty() {
-                *max_products.iter().min().unwrap()
-            } else {
-                *min_products.iter().min().unwrap()
-            };
-            let min_palindrome = Palindrome::new(min_product).unwrap();
-            let max_palindrome = Palindrome::new(*max_products.iter().max().unwrap()).unwrap();
-            result = Some((min_palindrome, max_palindrome));
+    for i in min..=max {
+        for j in i..=max {
+            let product = i * j;
+            if (min_product.is_none() || product < min_product.unwrap()) && is_palindrome(product) {
+                min_product = Some(product);
+            } else if (max_product.is_none() || product > max_product.unwrap())
+                && is_palindrome(product)
+            {
+                max_product = Some(product);
+            }
         }
     }
 
-    result
+    if let Some(min_product) = min_product && let Some(max_product) = max_product {
+        Some((
+            Palindrome::new(min_product).unwrap(),
+            Palindrome::new(max_product).unwrap(),
+        ))
+    } else {
+        None
+    }
 }
 
-fn find_palindrome_products_in_range(min: u64, max: u64) -> Vec<u64> {
-    (min..=max)
-        .flat_map(|x| (x..=max).map(move |y| x * y))
-        .filter(|&x| is_palindrome(x))
-        .collect::<Vec<u64>>()
-}
+// fn find_palindrome_products_in_range(min: u64, max: u64) -> Vec<u64> {
+//     (min..=max)
+//         .flat_map(|x| (x..=max).map(move |y| x * y))
+//         .filter(|&x| is_palindrome(x))
+//         .collect::<Vec<u64>>()
+// }
+
+// fn find_min_max_palindrome_products(min: u64, max: u64) -> Option<(u64, u64)> {
+//     (min..=max)
+//         .flat_map(|x| (x..=max).filter_map(move |y| if x * y > ))
+//         .filter(|&x| is_palindrome(x))
+//         .collect::<Vec<u64>>()
+// }
